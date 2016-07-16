4d88d8048677b902940872455bf915047bd94718
SébastienBourguet <bourguetsebastien@gmail.com>
2016-07-16 23:32:19 +0200

    Edition view update (save of start state).

---
+++ graphicimageitem.cpp
@@ -0,0 +1,6 @@
+#include "graphicimageitem.h"
+
+GraphicImageItem::GraphicImageItem()
+{
+
+}
---
+++ graphicimageitem.h
@@ -0,0 +1,12 @@
+#ifndef GRAPHICIMAGEITEM_H
+#define GRAPHICIMAGEITEM_H
+
+#include <QWidget>
+
+class GraphicImageItem : public QGraphicPixmapItem
+{
+public:
+    GraphicImageItem();
+};
+
+#endif // GRAPHICIMAGEITEM_H
